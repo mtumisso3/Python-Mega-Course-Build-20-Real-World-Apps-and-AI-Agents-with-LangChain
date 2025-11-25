a89ed9f13a5d9e7584e0567b03890482e23820e2
mtumisso3 <96927170+mtumisso3@users.noreply.github.com>
2025-11-25 05:00:50 +0000

    To DO List Application - Day 1

---
+++ To Do List.py
@@ -0,0 +1,4 @@
+# In this file, we will buld a To-Do List web application using Python.
+# Each day will add a piece to this app until it is complete - 20 days.
+# By day 20, we will have built the app which can have three versions: accessed via URL, a desktop GUI and a command line interface.
+# 
